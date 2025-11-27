dc1cc2d3f3acf118eba751401ba1588d896ee8fe
surmwill <william.surmak@gmail.com>
2025-11-27 13:43:33 -0500

    folder reorg

--- ProphetAR_Unity/Assets/Scripts/GameEvents/Framework/DeriveFrom/Listeners/IGameEventWithoutDataListenerExplicit.cs
+++ ProphetAR_Unity/Assets/Scripts/GameEvents/Framework/DeriveFrom/Listeners/IGameEventWithoutDataListenerExplicit.cs
@@ -1,9 +1,9 @@
 ﻿namespace ProphetAR
 {
     /// <summary>
-    /// Represents a game event that does not pass data. All game events with that requirement should implement this.
+    /// Represents a game event listener that does not take data. All game events with that requirement should implement this.
     ///
-    /// "Explicit" is for the extra type parameter which allows us to distinguish between different data-less listeners, all which
+    /// "Explicit" is for the extra type parameter which allows us to distinguish between different data-less listeners, all of which
     /// have the same method signature. Without the type parameter, we would not be able to distinguish between the different OnEvents
     /// </summary>
     public interface IGameEventWithoutDataListenerExplicit<T> : IGameEventWithoutDataListener where T : IGameEventWithoutDataListenerExplicit<T>
