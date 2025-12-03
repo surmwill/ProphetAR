6476397c9b68970d9af8d7e0cbac971326259c9d
surmwill <william.surmak@gmail.com>
2025-12-03 16:25:54 -0500

    add helpful world transforms

--- ProphetAR_Unity/Assets/Scripts/Grid/GridCell.cs
+++ ProphetAR_Unity/Assets/Scripts/Grid/GridCell.cs
@@ -29,6 +29,16 @@ namespace ProphetAR
         public Vector2 Coordinates => _coordinates;
 
         public Vector2 Dimensions => _parentGridSection.CellDimensions;
+
+        public Vector3 BotLeft => transform.position;
+
+        public Vector3 BotRight => transform.TransformPoint(transform.localPosition + Vector3.right * Dimensions.x);
+
+        public Vector3 Middle => transform.TransformPoint(transform.localPosition + Vector3.right * (Dimensions.x / 2f) + Vector3.forward * (Dimensions.y / 2f));
+
+        public Vector3 TopLeft => transform.TransformPoint(transform.localPosition + Vector3.forward * Dimensions.y);
+
+        public Vector3 TopRight => transform.TransformPoint(transform.localPosition + Vector3.right * Dimensions.x + Vector3.forward * Dimensions.y);
         
         public GridCell LeftCell { get; private set; }
         
