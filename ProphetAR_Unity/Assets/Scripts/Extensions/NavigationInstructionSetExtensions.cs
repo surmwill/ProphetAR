a117357c68577ea1c8b21d59f28e352541253598
surmwill <william.surmak@gmail.com>
2025-12-18 18:35:37 -0500

    commenting, adding tests

--- ProphetAR_Unity/Assets/Scripts/Extensions/NavigationInstructionSetExtensions.cs
+++ ProphetAR_Unity/Assets/Scripts/Extensions/NavigationInstructionSetExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using GridPathFinding;
 
@@ -8,7 +7,8 @@ namespace ProphetAR
     public static class NavigationInstructionSetExtensions
     {
         /// <summary>
-        /// Breaks the instruction set up into segments, each ending on (and including) the splitOn coordinates.
+        /// Breaks the instruction set up into segments, each segment ending on the splitOn coordinate. The start of each segment begins on the
+        /// end of the previous segment. Therefore, the individual segments overlap and can be joined together.
         /// </summary>
         public static List<NavigationInstructionSet> SplitOnCoordinates(this NavigationInstructionSet instructionSet, IEnumerable<(int row, int col)> splitOn)
         {
@@ -27,20 +27,38 @@ namespace ProphetAR
             int pathCoordinateIndex = 0;
             (int row, int col) currSplitOrigin = instructionSet.Origin;
             (int row, int col) currSplitOn = splitOnFirstToLast[splitIndex];
+
+            // Handle splitting on the origin.
+            // Usually a split involves starting from the end of the previous split and continuing on to the end of the current split. This implies a magnitude > 0.
+            // If we split on the origin, this a special case with magnitude = 0
+            if (currSplitOn == instructionSet.Origin)
+            {
+                splitInstructionSets.Add(new NavigationInstructionSet(instructionSet.Origin, instructionSet.Origin, new List<NavigationInstruction>()));
+                
+                splitIndex = GetNextSplitIndex(splitIndex + 1);
+                if (splitIndex == -1)
+                {
+                    return splitInstructionSets;
+                }
+                
+                currSplitOn = splitOnFirstToLast[splitIndex];
+                currSplitOrigin = instructionSet.Origin;
+            }
             
             foreach (NavigationInstruction instruction in instructionSet.PathToTarget)
             {
                 NavigationInstruction.NavigationDirection currDirection = instruction.Direction;
-                int lastSplitMagnitude = 0;
                 
+                int lastDirectionSplitMagnitude = 0;     
                 for (int currMagnitude = 1; currMagnitude <= instruction.Magnitude; currMagnitude++)
                 {
                     pathCoordinateIndex++;
+                    (int row, int col) pathCoordinates = instructionSet.PathCoordinates[pathCoordinateIndex];
                     
                     // Found split. Create a new instruction set for getting to that split
-                    if (instructionSet.PathCoordinates[pathCoordinateIndex] == currSplitOn)
+                    if (pathCoordinates == currSplitOn)
                     {
-                        currSplitInstructions.Add(new NavigationInstruction(currDirection, currMagnitude - lastSplitMagnitude));
+                        currSplitInstructions.Add(new NavigationInstruction(currDirection, currMagnitude - lastDirectionSplitMagnitude));       
                         splitInstructionSets.Add(new NavigationInstructionSet(currSplitOrigin, currSplitOn, currSplitInstructions));
                         
                         // Nothing left for splitting
@@ -50,12 +68,12 @@ namespace ProphetAR
                             break;
                         }
                         
-                        lastSplitMagnitude = currMagnitude;
+                        lastDirectionSplitMagnitude = currMagnitude;
                         
                         currSplitOn = splitOnFirstToLast[splitIndex];
-                        currSplitOrigin = instructionSet.PathCoordinates[pathCoordinateIndex];
-                        
-                        currSplitInstructions.Clear();
+                        currSplitOrigin = pathCoordinates;
+
+                        currSplitInstructions = new List<NavigationInstruction>();
                     }
                 }
 
@@ -65,10 +83,11 @@ namespace ProphetAR
                     break;
                 }
                 
-                // Change of direction
-                if (currSplitInstructions.Count > 0)
+                // Changed of direction
+                int remainingSplitMagnitude = instruction.Magnitude - lastDirectionSplitMagnitude;
+                if (remainingSplitMagnitude > 0)
                 {
-                    currSplitInstructions.Add(new NavigationInstruction(currDirection, instruction.Magnitude - lastSplitMagnitude));
+                    currSplitInstructions.Add(new NavigationInstruction(currDirection, remainingSplitMagnitude));   
                 }
             }
 
@@ -88,6 +107,10 @@ namespace ProphetAR
             return splitInstructionSets;
         }
 
+        /// <summary>
+        /// Breaks the instruction set up into segments which only go in a single direction.
+        /// Note the end of the previous segment forms the start of the next. Therefore the segments overlap and can be joined together 
+        /// </summary>
         public static List<NavigationInstructionSet> SplitOnDirectionChanges(this NavigationInstructionSet instructionSet, IEnumerable<(int row, int col)> alsoSplitOn = null)
         {
             List<(int row, int col)> lastCoordsBeforeDirectionChange = new List<(int row, int col)>();
--- ProphetAR_Unity/Assets/Tests/Editor/NavigationInstructionSetSplitting/TestNavigationInstructionSetSplitting.cs
+++ ProphetAR_Unity/Assets/Tests/Editor/NavigationInstructionSetSplitting/TestNavigationInstructionSetSplitting.cs
@@ -1,13 +1,65 @@
 ﻿using System.Collections.Generic;
 using GridPathFinding;
 using NUnit.Framework;
-
+using UnityEngine;
 using static GridPathFinding.NavigationInstruction;
 
 namespace ProphetAR.Tests.NavigationInstructionSetSplitting
 {
     public class TestNavigationInstructionSetSplitting
     {
+        [Test]
+        public void TestIndividualSplitting()
+        {
+            NavigationInstruction navigationInstructionDown1 = new NavigationInstruction(NavigationDirection.Down, 1);
+            NavigationInstruction navigationInstructionRight = new NavigationInstruction(NavigationDirection.Right, 2);
+            NavigationInstruction navigationInstructionDown2 = new NavigationInstruction(NavigationDirection.Down, 2);
+            
+            List<NavigationInstruction> navigationInstructions = new List<NavigationInstruction>(new[]
+            {
+                navigationInstructionDown1,
+                navigationInstructionRight,
+                navigationInstructionDown2,
+            });
+
+            NavigationInstructionSet navigationInstructionSet = new NavigationInstructionSet((0, 0), (1, 2), navigationInstructions);
+            List<NavigationInstructionSet> split = navigationInstructionSet.SplitOnCoordinates(new List<(int, int)>
+            {
+                (0, 0), (1, 0), (1, 1), (1, 2), (2, 2), (3, 2)
+            });
+            
+            // Each point should have its own split. Each split starts at the ending point from the previous split and moves on to its own ending point
+            Assert.IsTrue(split.Count == 6);
+            
+            // For the first split on the origin there is no previous ending point, and we have a special case with a magnitude 0 path
+            Debug.Log($"Checking split on origin (coordinate 0)");
+            NavigationInstructionSet firstInstructionSet = split[0];
+            
+            Assert.IsTrue(firstInstructionSet.Origin == (0, 0));
+            Assert.IsTrue(firstInstructionSet.PathCoordinates.Count == 1);
+            Assert.IsTrue(firstInstructionSet.PathCoordinates[0] == (0, 0));
+            Assert.IsTrue(firstInstructionSet.PathToTarget.Count == 0);
+            Assert.IsTrue(firstInstructionSet.Magnitude == 0);
+
+            // For the remaining splits they all start from the previous ending point (therefore having magnitude > 0)
+            for (int i = 1; i < split.Count; i++)
+            {
+                (int row, int col) pathCoordinates = navigationInstructionSet.PathCoordinates[i];
+                Debug.Log($"Checking split on coordinate {i}: {pathCoordinates}");
+
+                NavigationInstructionSet instructionSet = split[i];
+                NavigationInstructionSet prevInstructionSet = split[i - 1];
+                
+                Assert.IsTrue(instructionSet.Origin == prevInstructionSet.Target);
+                Assert.IsTrue(instructionSet.Target == pathCoordinates);
+                Assert.IsTrue(instructionSet.PathToTarget.Count == 1, instructionSet.PathToTarget.Count.ToString());
+                Assert.IsTrue(instructionSet.PathCoordinates.Count == 2, instructionSet.PathCoordinates.Count.ToString());
+                Assert.IsTrue(instructionSet.PathCoordinates[0] == prevInstructionSet.Target);
+                Assert.IsTrue(instructionSet.PathCoordinates[1] == pathCoordinates, instructionSet.PathCoordinates[1].ToString() + " " + pathCoordinates);
+                Assert.IsTrue(instructionSet.Magnitude == 1);
+            }
+        }
+        
         [Test]
         public void TestSplitting()
         {
@@ -41,11 +93,11 @@ namespace ProphetAR.Tests.NavigationInstructionSetSplitting
             // Split at (2, 2): the start of the second step of the path -> one off the end of the path
             NavigationInstructionSet setTwo = split[1];
             
-            Assert.IsTrue(setTwo.Origin == (1, 0));
-            Assert.IsTrue(setTwo.Target == (1, 2));
-            Assert.IsTrue(setTwo.PathCoordinates.Count == 4);
+            Assert.IsTrue(setTwo.Origin == (1, 0), setTwo.Origin.ToString());
+            Assert.IsTrue(setTwo.Target == (2, 2), setTwo.Target.ToString());
+            Assert.IsTrue(setTwo.PathCoordinates.Count == 4, setTwo.PathCoordinates.Count.ToString());
             
-            Assert.IsTrue(setTwo.PathToTarget.Count == 2);
+            Assert.IsTrue(setTwo.PathToTarget.Count == 2, setTwo.PathToTarget.Count.ToString());
             Assert.IsTrue(setTwo.PathToTarget[0].Direction == NavigationDirection.Right);
             Assert.IsTrue(setTwo.PathToTarget[0].Magnitude == 2);
             
@@ -77,12 +129,51 @@ namespace ProphetAR.Tests.NavigationInstructionSetSplitting
             Assert.IsTrue(setOne.Target == (1, 0));
             
             NavigationInstructionSet setTwo = split[1];
-            Assert.IsTrue(setTwo.Origin == (1, 1));
+            Assert.IsTrue(setTwo.Origin == (1, 0));
             Assert.IsTrue(setTwo.Target == (1, 2));
             
             NavigationInstructionSet setThree = split[2];
             Assert.IsTrue(setThree.Origin == (1, 2));
             Assert.IsTrue(setThree.Target == (3, 2));
+        }
+        
+        [Test]
+        public void TestSplitOnEmptyInstructions()
+        {
+            NavigationInstructionSet navigationInstructionSet = new NavigationInstructionSet((0, 0), (0, 0), new List<NavigationInstruction>());
+            
+            List<NavigationInstructionSet> split = navigationInstructionSet.SplitOnCoordinates(new List<(int, int)> { (0, 0) });
+            Assert.IsTrue(split.Count == 1);
+            Assert.IsTrue(split[0].PathCoordinates.Count == 1);
+            Assert.IsTrue(split[0].PathCoordinates[0] == (0, 0));
+            Assert.IsTrue(split[0].PathToTarget.Count == 0);
+
+            navigationInstructionSet.SplitOnDirectionChanges();
+            Assert.IsTrue(split.Count == 1);
+            Assert.IsTrue(split[0].PathCoordinates.Count == 1);
+            Assert.IsTrue(split[0].PathCoordinates[0] == (0, 0));
+            Assert.IsTrue(split[0].PathToTarget.Count == 0);
+        }
+        
+        [Test]
+        public void TestEmptySplitOn()
+        {
+            NavigationInstruction navigationInstructionDown1 = new NavigationInstruction(NavigationDirection.Down, 1);
+            NavigationInstruction navigationInstructionRight = new NavigationInstruction(NavigationDirection.Right, 2);
+            NavigationInstruction navigationInstructionDown2 = new NavigationInstruction(NavigationDirection.Down, 2);
+
+            List<NavigationInstruction> navigationInstructions = new List<NavigationInstruction>(new[]
+            {
+                navigationInstructionDown1,
+                navigationInstructionRight,
+                navigationInstructionDown2
+            });
+
+            NavigationInstructionSet navigationInstructionSet = new NavigationInstructionSet((0, 0), (3, 2), navigationInstructions);
+            List<NavigationInstructionSet> split = navigationInstructionSet.SplitOnCoordinates(new List<(int row, int col)>());
+            
+            Assert.IsTrue(split.Count == 1);
+            Assert.IsTrue(split[0] == navigationInstructionSet);
         }
     }
 }
