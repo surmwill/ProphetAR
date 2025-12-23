2b5df2e9042179e332e6d860590f33a01476df1a
surmwill <william.surmak@gmail.com>
2025-12-23 16:10:42 -0500

    wip adding debug indicator to spawn point

--- ProphetAR_Unity/Assets/Scripts/Grid/SpecialCells/GridCellCharacterSpawnPoint.cs
+++ ProphetAR_Unity/Assets/Scripts/Grid/SpecialCells/GridCellCharacterSpawnPoint.cs
@@ -11,6 +11,9 @@ namespace ProphetAR
         [SerializeField]
         private CharacterSpawnPoint _spawnPoint = default;
 
+        [SerializeField]
+        private GameObject _debugIndicator = null;
+
         public string LevelConfigEditedBy => $"Character spawn point. Player Index: {_spawnPoint.PlayerIndex}, Coordinates: {_spawnPoint.Coordinates}";
 
         private Level Level => _gridCellContent.Cell.GridSection.ParentGrid.Level;
@@ -22,6 +25,11 @@ namespace ProphetAR
         
         private void OnValidate()
         {
+            if (_gridCellContent == null)
+            {
+                _gridCellContent = GetComponentInChildren<GridCellContent>();
+            }
+            
             _spawnPoint.Coordinates = GetComponentInParent<GridCell>().Coordinates;
         }
         
