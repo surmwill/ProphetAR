726ba67bf74101f51094e4df42c86f976c93410f
surmwill <william.surmak@gmail.com>
2026-01-27 22:52:16 -0500

    adding object drawer

---
+++ ProphetAR_Unity/Assets/Scripts/AR/RaycastDrawer/ARRaycastDrawerObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ProphetAR
+{
+    public class ARRaycastDrawerObject : ARRaycastDrawer
+    {
+        public override void DrawRaycast(RaycastHit raycastHit)
+        {
+            Vector3 up = ARManager.Instance.GroundPlaneManager.Content != null ? ARManager.Instance.GroundPlaneManager.Content.up : Vector3.up;
+            transform.position = raycastHit.point + up * 0.1f;
+            transform.forward = Vector3.ProjectOnPlane(raycastHit.point - ARManager.Instance.Camera.transform.position, up);
+        }
+    }
+}
